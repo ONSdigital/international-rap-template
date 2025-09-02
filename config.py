5e95d5ef9e88df04a2f4b9a887e76c7d5888c604
baizaa <amanda.baizan.edge@ons.gov.uk>
2025-09-02 13:29:16 +0100

    Added a config file to set paths and parameters for example data

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+# Configuration file for pipeline scripts
+
+data_dir = "demonstration_data/"
+data_path = "demonstration_data/input_data.xlsx"
+revised_data_path = "demonstration_data/input_data_revisedXXXXXXXX.xlsx"
+output_path = 'data/output_results.xlsx'
